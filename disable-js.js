8b62a4060f66b544debb8f75a6a013bc6d6c617d
entroop1 <radziejewski@gmail.com>
2017-07-06 15:05:57 +0200

    1
    1

--- disable-js.js
+++
@@ -1,3 +0,0 @@
-if (window.width > 779) {
- $('head').append("<script src="jquery.parallax-scroll-master/jquery.parallax-scroll-master/js/jquery.parallax-scroll.js"></script>");
-}
--- play.js
+++
@@ -1,7 +0,0 @@
-$('video').each(function(){
-    if ($(this).is(":in-viewport")) {
-        $(this)[0].play();
-    } else {
-        $(this)[0].pause();
-    }
-})
